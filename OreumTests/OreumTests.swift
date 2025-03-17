7ba49b620a0fc90fda37155dc64cdcbf5352b66b
eseungh <eseungh@gmail.com>
2025-03-18 00:34:30 +0900

    Initial Commit

---
+++ OreumTests/OreumTests.swift
@@ -0,0 +1,17 @@
+//
+//  OreumTests.swift
+//  OreumTests
+//
+//  Created by Seungho on 3/18/25.
+//
+
+import Testing
+@testable import Oreum
+
+struct OreumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
